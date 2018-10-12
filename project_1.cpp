adc02071e1516bb297ad5d1e0f6e6aafcda7cac5
Lai <blossomon54@gmail.com>
2018-10-12 21:07:38 +0800

    test file I/O

--- project_1.cpp
+++ project_1.cpp
@@ -1,13 +1,25 @@
 #include <iostream> 
+#include <fstream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout<<"argc = "<<argc<<endl;
     cout<<"argv : ";
-    for(int i = 0; i < argc; i++) {
-        cout<<argv[i]<<" ";
+    for (int i = 0; i < argc; i++) {
+        cout << argv[i] << " ";
     }
     cout<<endl;
+    ifstream inFile(argv[1], ios::in);
+    if(!inFile) {
+        cout << "cannot open";
+        return 1;
+    }
+    int x;
+    
+    while(!inFile.eof()) {
+        inFile >> x;
+        cout << x << endl;
+    }
 
     return 0;
 }
