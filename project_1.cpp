c4a109e4822ae4b870c9fd39924ad0efa9165a23
Lai <blossomon54@gmail.com>
2018-10-12 21:33:45 +0800

    store file in local matrix

--- project_1.cpp
+++ project_1.cpp
@@ -1,24 +1,31 @@
 #include <iostream> 
 #include <fstream>
 using namespace std;
+int matrix[1000][1000];
 
 int main(int argc, char *argv[]) {
-    cout<<"argc = "<<argc<<endl;
-    cout<<"argv : ";
-    for (int i = 0; i < argc; i++) {
-        cout << argv[i] << " ";
-    }
-    cout<<endl;
+    int row, col;
+    
     ifstream inFile(argv[1], ios::in);
     if(!inFile) {
         cout << "cannot open";
         return 1;
     }
-    int x;
-    
-    while(!inFile.eof()) {
-        inFile >> x;
-        cout << x << endl;
+
+    inFile >> row;
+    inFile >> col;
+
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            inFile >> matrix[i][j];
+        }
+    }
+
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            cout << matrix[i][j];
+        }
+        cout << endl;
     }
 
     return 0;
